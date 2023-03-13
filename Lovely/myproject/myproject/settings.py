205b0c694bfaa4f6c242aeede648b7437f27d069
Banu <banuthabsheera@gmail.com>
2023-03-13 11:22:20 +0530

    database config

--- Lovely/myproject/myproject/settings.py
+++ Lovely/myproject/myproject/settings.py
@@ -101,11 +101,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-        
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': ,
+        'USER' : ,
+        'PASSWORD' : ,
+        'HOST' : 'localhost'
     }
-
 }
 
 # Password validation
