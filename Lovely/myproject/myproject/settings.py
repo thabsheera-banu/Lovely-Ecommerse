5e6a5910ed450f34554e396ccb7449656998a70c
Banu <banuthabsheera@gmail.com>
2023-03-13 10:59:00 +0530

    settings modified

--- Lovely/myproject/myproject/settings.py
+++ Lovely/myproject/myproject/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import debug_toolbar
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
