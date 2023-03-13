228d8979e6db9433f62ef34f1deb464b4d4018b0
Banu <banuthabsheera@gmail.com>
2023-03-13 12:01:27 +0530

    database updated

--- Lovely/myproject/myproject/settings.py
+++ Lovely/myproject/myproject/settings.py
@@ -99,12 +99,21 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': BASE_DIR / 'db.sqlite3',
+        
+#     }
+
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': ,
-        'USER' : ,
-        'PASSWORD' : ,
+        'NAME': 'lovely',
+        'USER' : 'postgres',
+        'PASSWORD' : '123456',
         'HOST' : 'localhost'
     }
 }
