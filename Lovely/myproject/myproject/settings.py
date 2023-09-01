0d9a141f2b9516e084c6ceac3e75b0d480375bf1
Banu <banuthabsheera@gmail.com>
2023-09-01 13:11:06 +0530

    changes in db

--- Lovely/myproject/myproject/settings.py
+++ Lovely/myproject/myproject/settings.py
@@ -96,14 +96,14 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#          'NAME': BASE_DIR / 'db.sqlite3',
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
         
-#     }
+    }
 
-# }
+}
 
 # DATABASES = {
 #     'default': {
@@ -115,15 +115,15 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'lovely',
-        'USER' : 'lovely_user',
-        'PASSWORD' : 'Ygmqw4hd74FIgFEfd5bJgpaChQQMjWqi',
-        'HOST' : 'dpg-cjooqdj6fquc73f0hrog-a'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'lovely',
+#         'USER' : 'lovely_user',
+#         'PASSWORD' : 'Ygmqw4hd74FIgFEfd5bJgpaChQQMjWqi',
+#         'HOST' : 'dpg-cjooqdj6fquc73f0hrog-a'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
