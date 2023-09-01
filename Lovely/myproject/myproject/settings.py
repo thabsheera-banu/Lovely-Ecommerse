b7bbe06b461fc80703d827dd7d0def48d5158343
Banu <banuthabsheera@gmail.com>
2023-09-01 15:21:28 +0530

    template changes

--- Lovely/myproject/myproject/settings.py
+++ Lovely/myproject/myproject/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
